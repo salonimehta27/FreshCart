52c823a614ec127afc352193a3c101d7c580f822
Saloni Mehta <salonimehta2127@gmail.com>
2023-05-05 21:41:45 -0400

    Beginning

---
+++ back-end/server.py
@@ -0,0 +1,11 @@
+from flask import (Flask, render_template, request, flash, session, redirect) 
+from model import connect_to_db, db
+
+import crud
+app = Flask(__name__)
+app.secret_key = "dev"
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
